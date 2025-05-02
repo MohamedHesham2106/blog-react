2fbe62f424a74568735bcac7cbac516efcd51a35
Mohamed Hesham <moohisham1710@gmail.com>
2025-05-02 15:13:13 +0300

    fix: update favicon link and remove unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-import { StrictMode } from "react";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
